63de74b92fc148ac8297f0e199942e9a2c423431
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-05-13 14:11:59 +0200

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -3,7 +3,6 @@ echo 'docker is available with version:'
 docker version
 
 echo 'Installing .NET Core...'
-
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
